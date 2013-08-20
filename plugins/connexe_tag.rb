e6371c22e97f6d86dcd628b0def54dd27f4d62e5
lkdjiin <xavier.nayrac@gmail.com>
2013-08-20 10:23:40 +0200

    mardi 20 août 2013, 10:23:40 (UTC+0200)

---
+++ plugins/connexe_tag.rb
@@ -0,0 +1,40 @@
+module Jekyll
+  class ConnexeTag < Liquid::Tag
+
+    def initialize(tagname, str, tokens)
+    end
+
+    def render(context)
+      categories = get_categories(context)
+      posts = get_posts(context)
+      # 1 Supprimer les posts qui n'ont pas au moins une
+      #   catégorie commune.
+      # posts = posts.select do |post|
+      #   result = false
+      #   categories.each do |cat|
+      #     result = true if post.categories.include?(cat)
+      #   end
+      #   result
+      # end
+      # posts.inspect
+
+      # 2 Supprimer le post courant.
+
+      # 3 Attribuer un poid.
+
+      categories.size
+    end
+
+    private
+
+    def get_categories(context)
+      context.environments.first["page"]["categories"]
+    end
+    def get_posts(context)
+      # context.registers[:site].posts.last.url
+      context.registers[:site].posts
+    end
+
+  end
+end
+Liquid::Template.register_tag('connexe', Jekyll::ConnexeTag)
