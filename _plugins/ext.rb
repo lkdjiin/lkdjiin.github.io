b2b7d3d7779854bd44e329ec02e862b9faa057b2
Xavier Nayrac <xavier.nayrac@gmail.com>
2026-03-31 18:31:45 +0200

    Dates in french

--- _plugins/ext.rb
+++ _plugins/ext.rb
@@ -6,6 +6,21 @@ module Jekyll
       hash.keys.sort_by { |k| k.to_s.downcase }.map { |k| [k, hash[k]] }
     end
   end
+
+  module FrenchDateFilter
+    FRENCH_MONTHS = [
+      'janvier', 'février', 'mars', 'avril', 'mai', 'juin',
+      'juillet', 'août', 'septembre', 'octobre', 'novembre', 'décembre'
+    ].freeze
+
+    def french_date(date)
+      day = date.strftime('%d').to_i
+      month = FRENCH_MONTHS[date.strftime('%-m').to_i - 1]
+      year = date.strftime('%Y')
+      "#{day} #{month} #{year}"
+    end
+  end
 end
 
 Liquid::Template.register_filter(Jekyll::SortHashKeysFilter)
+Liquid::Template.register_filter(Jekyll::FrenchDateFilter)
